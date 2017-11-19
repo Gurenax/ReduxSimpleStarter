bc07a78ee80873a48c4a2ed6e32b2b1f3c2c2a73
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-19 21:19:46 +1100

    Add onChange handler for search bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Search Bar component
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+}
 
 // Export Search Bar to index.js
 export default SearchBar;
