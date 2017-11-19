450a1a1aac70f552be170116d784b1cb2a1be2c7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-19 20:56:25 +1100

    Add search bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Search Bar component
+const SearchBar = () => {
+  return <input />;
+};
+
+// Export Search Bar to index.js
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Our components
+import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
 // Create a new component.
 // This component should produce some HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML
