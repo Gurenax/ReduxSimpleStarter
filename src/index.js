adfad7a74410775f1b83d74f6faf883e1eb73e1d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-20 01:10:05 +1100

    Add ability to search a video

--- src/index.js
+++ src/index.js
@@ -21,10 +21,14 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
-        selectedVideo: videos[0]
+        selectedVideo: videos[0] // Select the first video by default
       });
     });
   }
@@ -32,7 +36,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           // onVideoSelect={chosenVideo => this.setState({selectedVideo: chosenVideo})}
