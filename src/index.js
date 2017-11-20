1874df18200b4c6a57da15a4b58d8c644ef1a3c7
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-21 00:45:20 +1100

    Refactor lodash to import debounce only

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { debounce } from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
